842bc670ec318ab2f7748d1088e098e783ca78fd
fahadmp955 <fahad@getmoneyplant.ai>
2025-11-26 13:33:39 +0530

    Added status fetching, transaction fetching, view on chain functionality

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
   const [transactionPayload, setTransactionPayload] = useState("");
   const [signedPayload, setSignedPayload] = useState("");
   const [submitResponse, setSubmitResponse] = useState("");
+  const [txnStatusResponse, setTxnStatusResponse] = useState("");
+  const [jobStatusResponse, setJobStatusResponse] = useState("");
   const [privateKey, setPrivateKey] = useState("");
 
   // NEW LOADING STATES
   const [loadingApiCall, setLoadingApiCall] = useState(false);
   const [loadingSign, setLoadingSign] = useState(false);
   const [loadingSubmit, setLoadingSubmit] = useState(false);
+  const [loadingJobStatus, setLoadingJobStatus] = useState(false);
+  const [loadingTxnStatus, setLoadingTxnStatus] = useState(false);
 
   const handleApiCall = async () => {
     setLoadingApiCall(true);
@@ -100,6 +104,48 @@ function App() {
     }
     setLoadingSubmit(false);
   };
+  const handleGetJobStatus = async () => {
+    setLoadingJobStatus(true);
+    try {
+      const convertedObj = JSON.parse(signedPayload);
+      const res = await fetch(
+        `${process.env.REACT_APP_API_URL}/jobs/status?jobId=${convertedObj.jobId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(convertedObj),
+        }
+      );
+      const data = await res.json();
+      setJobStatusResponse(JSON.stringify(data, null, 2));
+    } catch (error) {
+      setJobStatusResponse(error.message);
+    }
+    setLoadingJobStatus(false);
+  };
+  const handleGetTxnStatus = async () => {
+    setLoadingTxnStatus(true);
+    try {
+      const convertedObj = JSON.parse(signedPayload);
+      const res = await fetch(
+        `${process.env.REACT_APP_RELAYER_API_URL}/api/v1/relayers/glhf-example/transactions/${convertedObj.jobId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(convertedObj),
+        }
+      );
+      const data = await res.json();
+      setTxnStatusResponse(JSON.stringify(data, null, 2));
+    } catch (error) {
+      setTxnStatusResponse(error.message);
+    }
+    setLoadingTxnStatus(false);
+  };
 
   return (
     <div>
@@ -172,6 +218,67 @@ function App() {
           {loadingSubmit ? "Submitting..." : "Submit Transaction"}
         </button>
         <pre>{submitResponse}</pre>
+      </div>
+
+      <div>
+        {submitResponse.jobId && (
+          <>
+            <h2>4. Get Job Status</h2>
+            <textarea
+              placeholder="Job Id"
+              value={submitResponse.jobId}
+              readOnly
+            />
+            <br />
+
+            {/* DISABLED BUTTON */}
+            <button onClick={handleGetJobStatus} disabled={loadingJobStatus}>
+              {loadingJobStatus ? "Fetching..." : "Get Job Status"}
+            </button>
+
+            <pre>{jobStatusResponse}</pre>
+          </>
+        )}
+      </div>
+
+      <div>
+        {jobStatusResponse.transactionId && (
+          <>
+            <h2>5. Get Transaction Status</h2>
+
+            <textarea
+              placeholder="Transaction ID"
+              value={jobStatusResponse.transactionId}
+              readOnly
+            />
+            <br />
+
+            {/* DISABLED BUTTON */}
+            <button onClick={handleGetTxnStatus} disabled={loadingSubmit}>
+              {loadingSubmit ? "Fetching..." : "Get Transaction Status"}
+            </button>
+
+            <pre>{txnStatusResponse}</pre>
+          </>
+        )}
+      </div>
+
+      <div>
+        {txnStatusResponse.transactionHash && (
+          <>
+            <h2>6. Verify on Chain</h2>
+            <button
+              onClick={() =>
+                window.open(
+                  `https://glhf-testnet.explorer.caldera.xyz/tx/${txnStatusResponse.transactionHash}`,
+                  "_blank"
+                )
+              }
+            >
+              View Transaction
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
