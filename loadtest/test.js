27658e02fc34c1dae0bbacbe6fcb5bd8ded61bb1
Durga Prasad Mudhuru <mdprasadeng@gmail.com>
2025-12-12 13:51:13 +0530

    feat: add log parsing functionality and update dependencies

--- loadtest/test.js
+++ loadtest/test.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import exec from 'k6/execution';
 
 export default function () {
     const backendhost = "https://cricket-manager-backend-894136132206.asia-south1.run.app/"
@@ -14,16 +15,27 @@ export default function () {
     //     }
     // ), { headers: { 'Content-Type': 'application/json' } });
     // const payload = r1.json();
-
-    const r1 = http.post(backendhost + "cricketer/add-supply", JSON.stringify(
+    const currentIteration = exec.scenario.iterationInTest + 1;
+    console.log("Current Iteration: " + currentIteration);
+    const r1 = http.post(backendhost + "cricketer/create-token", JSON.stringify(
         {
-            "contractAddress": "0x9F4DfDb029350C77da2cB024c68b81bE80DAc0a7",
+            "name": "VKT" + currentIteration,
             "adminWalletAddress": "0xb46be61fb0dd88fb5eb2f5ad7163454304314b82",
-            "additionalSupply": 1
+            "symbol": "VKC" + currentIteration,
+            "initialSupply": 10
         }
     ), { headers: { 'Content-Type': 'application/json' } });
     const payload = r1.json();
-    console.log(r1.json());
+
+
+    // const r1 = http.post(backendhost + "cricketer/add-supply", JSON.stringify(
+    //     {
+    //         "contractAddress": "0x9F4DfDb029350C77da2cB024c68b81bE80DAc0a7",
+    //         "adminWalletAddress": "0xb46be61fb0dd88fb5eb2f5ad7163454304314b82",
+    //         "additionalSupply": 1
+    //     }
+    // ), { headers: { 'Content-Type': 'application/json' } });
+    // const payload = r1.json();
 
 
     const r2 = http.post(demohost + "api/sign", JSON.stringify({
@@ -32,8 +44,6 @@ export default function () {
     }), { headers: { 'Content-Type': 'application/json' } });
 
     const jobId = r2.json().jobId;
-    console.log("JobID: " + jobId);
-    console.log(r2.json());
 
     const r3 = http.post(backendhost + "jobs/submit-transaction", JSON.stringify(
         r2.json()
